f510efdeb86b298640dd2736ec61ec61e5fd494b
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-03-09 11:40:48 +0900

    Bug fixed

--- app/src/main/java/com/gardenlab/doitmission15/MainActivity.java
+++ app/src/main/java/com/gardenlab/doitmission15/MainActivity.java
@@ -3,10 +3,16 @@ package com.gardenlab.doitmission15;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout userInfo;
+    Animation leftAnimation;
+    Animation rightAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
         userInfo = findViewById(R.id.userInfo);
 
+        leftAnimation = AnimationUtils.loadAnimation(this,R.anim.to_left);
+        rightAnimation = AnimationUtils.loadAnimation(this,R.anim.to_right);
 
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userInfo.setVisibility(View.VISIBLE);
+                userInfo.startAnimation(leftAnimation);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userInfo.startAnimation(rightAnimation);
+            }
+        });
 
     }
 }
