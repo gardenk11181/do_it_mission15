9c1f46371a2bd5d66cfd172155c47e46b54d7358
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-03-09 11:29:43 +0900

    Add Animation

--- app/src/main/java/com/gardenlab/doitmission15/MainActivity.java
+++ app/src/main/java/com/gardenlab/doitmission15/MainActivity.java
@@ -3,12 +3,19 @@ package com.gardenlab.doitmission15;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout userInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInfo = findViewById(R.id.userInfo);
+
+
+
     }
 }
